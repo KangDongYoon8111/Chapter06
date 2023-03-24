bd338de3d7075e94b505210c6d21079ead4c6ec1
동윤 강 <davidkang8111@gmail.com>
2023-03-24 15:36:09 +0900

    202303241536

---
+++ UsingOut/MainApp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace UsingOut
+{
+    class MainApp
+    {
+        static int[] Divide(int a, int b)
+        {
+            int[] abc = new int[2];
+            abc[0] = a / b;
+            abc[1] = a % b;
+
+            return abc;
+        }
+
+        static void Main(string[] args)
+        {
+            int a = 20;
+            int b = 3;
+            int[] cde;
+            //int c;
+            //int d;
+
+            cde = Divide(a, b);
+
+            Console.WriteLine("a:{0}, b:{1}", a, b);
+            Console.WriteLine($"a:{a}, b:{b}, a/b:{cde[0]}, a%b:{cde[1]}");
+        }
+    }
+}
